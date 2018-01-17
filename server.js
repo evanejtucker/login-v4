24c3af32bf3bccc9091d05784dd6b300afd56452
Evan Tucker <eej.tucker@gmail.com>
2018-01-17 14:29:29 -0700

    added handlebars

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
-var profile = require('./controller/routes/profile')
+var profile = require('./controller/routes/profile');
+var exphbs  = require('express-handlebars');
+
+    // handlebars
+    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
 
     // express middleware
     app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,7 +21,8 @@ var profile = require('./controller/routes/profile')
 
 
 app.get('/', (req, res, next) =>  {
-    res.send('Hello World!')
+    // res.send('Hello World!')
+    res.render('index');
 });
 
 app.listen(port, () => {
